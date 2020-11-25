c2156683028e83c0861ca29ffd8c9ad91ac6e145
inquityy1 <nebojsanikolic12350@yahoo.com>
2020-11-26 00:01:45 +0100

    js scrollTop event

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
 $(document).ready(function(){
+	
+	//toggle button
 	$('.btn').click(function(){
 		$('.nav-links').slideToggle(3000);
 		$('.btn').toggleClass('change');
 	})
+	//transparent background
+	
+	$(window).scroll(function(){
+		
+		let position = $(window).scrollTop();
+		
+		if(position >= 100){
+			$('nav, nav-container').addClass('navBackground')
+		}
+		else{
+			$('nav, nav-container').removeClass('navBackground')
+			
+		}
+		
+	})
+	
 })
